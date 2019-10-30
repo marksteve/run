200d1c35abe2c3ad3dae7ca45dc4d4ebc7686aa2
Mark Steve Samson <hello@marksteve.com>
2019-10-30 15:19:01 +0800

    Update email subject

--- email-form/main.py
+++ email-form/main.py
@@ -21,6 +21,12 @@ def render_rows(form):
 
 
 def create_message(form):
+    subject = "New submission"
+    for key in "name", "Name", "email", "Email":
+        name = form.get(key)
+        if name:
+            subject += f" from {name}"
+            break
     html = f"""
 <h1>New submission</h1>
 <table cellpadding=10 cellspacing=0>
@@ -30,7 +36,7 @@ def create_message(form):
     return {
         "from": os.environ["EMAIL_FROM"],
         "to": os.environ["EMAIL_TO"],
-        "subject": "New submission",
+        "subject": subject,
         "html": html,
     }
 
